c81073913313cdd351928eda5c82537191bed456
latha-sree-b <lathasree.balappagari@gmail.com>
2026-09-02 10:56:21 +0530

    This is my First commit

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,8 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
        //Write your code here
+       System.out.println("welcome to java");
+       System.out.println("I am in java Track");
+       
     }
 }
