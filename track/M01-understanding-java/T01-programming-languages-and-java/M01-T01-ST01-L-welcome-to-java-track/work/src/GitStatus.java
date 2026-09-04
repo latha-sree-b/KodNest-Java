577475c905537c6f373ce990c37f0a53dfd8efc7
latha-sree-b <lathasree.balappagari@gmail.com>
2026-09-04 14:11:16 +0530

    GitStatus

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/GitStatus.java
@@ -0,0 +1,9 @@
+public class GitStatus {
+    public static void main(String[] args) {
+        // Display the repository cloning status.
+        System.out.println("Repository: Cloned Sucessfully");
+
+        // Display Whether the project is ready.
+        System.out.println("Project Status: Ready for Development");
+    }
+}
