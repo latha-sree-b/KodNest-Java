35b4016474e33a0bcadff6755ea726615514df96
latha-sree-b <lathasree.balappagari@gmail.com>
2026-09-04 14:06:19 +0530

    Commitment

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Commitment.java
@@ -0,0 +1,9 @@
+public class Commitment {
+    public static void main(String[] args) {
+        // Display the Student's learning track.
+        System.out.println("Track: Java Development");
+        // Display the student's daily committed hours to learning.
+        System.out.println("Daily Practice: 8 Hours");
+    }
+}
+    
