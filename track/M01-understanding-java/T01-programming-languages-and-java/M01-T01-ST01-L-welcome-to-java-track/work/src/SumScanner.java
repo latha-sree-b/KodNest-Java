b8e24e5f1f0450e910b950ce70f0deb3a8e85880
latha-sree-b <lathasree.balappagari@gmail.com>
2026-09-06 09:02:10 +0530

    scanneradd

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/SumScanner.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class SumScanner {
+    public static void main(String[] args) {
+        // Create a Scanner object to read input from the console.
+        Scanner sc = new Scanner(System.in);
+        // Read the first integer from the user and store it in variable 'a'.
+        int a = sc.nextInt();
+        // Read the second integer from the user and store it in variable 'b'.
+        int b = sc.nextInt();
+        // Calculate the sum of 'a' and 'b' and print the result.
+        System.out.println("sum of a+b is " + (a+b));
+        sc.close();
+    }
+}
