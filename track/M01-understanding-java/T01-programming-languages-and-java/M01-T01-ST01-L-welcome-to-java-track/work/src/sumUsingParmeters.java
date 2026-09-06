999ef23c487dfa0fd452ce0934c33e882d18e3d2
latha-sree-b <lathasree.balappagari@gmail.com>
2026-09-06 09:08:55 +0530

    sumusingParameters

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/sumUsingParmeters.java
@@ -0,0 +1,16 @@
+public class sumUsingParmeters {
+    public static void main(String[] args) {
+        // Declare and initialize two integer variables.
+        int a = 10;
+        int b = 20;
+        // Call the sum method with 'a' and 'b' as arguments.
+        sum(a, b);
+    }
+
+    // Define a method 'sum' that accepts two integers 'a' and 'b' as parameters.
+    public static void sum(int a, int b) {
+        // Print the sum of 'a' and 'b'.
+        System.out.println(a+b);
+    }
+
+}
