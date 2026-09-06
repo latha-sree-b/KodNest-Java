fd6aecb6bcebcc7957e6f72f8182c219f444e45d
latha-sree-b <lathasree.balappagari@gmail.com>
2026-09-06 09:11:00 +0530

    sumWithoutparameters

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/sumWithoutparmeters.java
@@ -0,0 +1,13 @@
+public class sumWithoutparmeters {
+    public static void main(String[] args) {
+        // Call the sum method without arguments.
+        sum();
+    }
+    public static void sum() {
+        // Declare and initialize two integer variables.
+        int a=10;
+        int b=20;
+        // Calculate the sum of 'a' and 'b' and print the result.
+        System.out.println(a+b);
+    }
+}
