acb2bd5891c784ac2632e0178459d00ef323e0a3
latha-sree-b <lathasree.balappagari@gmail.com>
2026-09-06 08:56:18 +0530

    sumUsingOperators

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/sumUsingOperators.java
@@ -0,0 +1,13 @@
+public class sumUsingOperators {
+    public static void main(String[] args) {
+        // Define two integer variables with values 10 and 20 respectively.
+        int a=10;
+        int b=20;
+
+        // Use the addition operator to calculate the sum of 'a' and 'b'.
+        int result=a+b;
+
+        // Print the sum of 'a' and 'b' to the console.
+        System.out.println("sum of a and b is "+result);
+    }
+}
