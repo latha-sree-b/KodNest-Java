fdc5871cc6c6a07cafc2e3018218eecd16f584c1
latha-sree-b <lathasree.balappagari@gmail.com>
2026-09-07 08:57:47 +0530

    Add dollar to rupees and subtraction programs

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Rectangleperimeter.java
@@ -0,0 +1,14 @@
+public class Rectangleperimeter {
+    public static void main(String[] args) {
+        // Declare and initialize the length and width of a rectangle
+        int length=10;
+        int width=10;
+
+        // Calculate the perimeter of the rectangle using the formula 2 * (length + width)
+        int perimeter=2*(length+width);
+
+        // Print the perimeter of the rectangle
+        System.out.println("perimeter is "+perimeter);
+
+    }
+}
