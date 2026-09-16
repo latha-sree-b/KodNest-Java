c2e2c275fac7c29f55a3107a3b097fffffdafa37
latha-sree-b <lathasree.balappagari@gmail.com>
2026-09-16 10:00:00 +0530

    salmanProfile

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/SalmanLearningProfile.java
@@ -0,0 +1,9 @@
+public class SalmanLearningProfile {
+    public static void main(String[] args) {
+        String LearnerName="Salman";
+        int dailyPractice=8;
+        System.out.println("Learner Name: " + LearnerName);
+        System.out.println("Daily Practice: " + dailyPractice);
+        
+    }
+}
