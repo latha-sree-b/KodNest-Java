83ab35237bfa234726958c376fe51e63ffe2cbea
latha-sree-b <lathasree.balappagari@gmail.com>
2026-09-18 18:52:50 +0530

    One To N numbers

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/OneToNNumbers.java
@@ -0,0 +1,10 @@
+public class OneToNNumbers {
+    public static void main(String[] args) {
+        int limit = 5;
+
+        // Print numbers from 1 through limit
+        for (int i = 1; i <= limit; i++) {
+            System.out.println(i);
+        }
+    }
+}
