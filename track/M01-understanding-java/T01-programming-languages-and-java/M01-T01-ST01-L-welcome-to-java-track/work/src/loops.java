9d8415bd6226333d22fc360b031c6a994bcbf2fd
latha-sree-b <lathasree.balappagari@gmail.com>
2026-09-18 19:05:44 +0530

    loops

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/loops.java
@@ -0,0 +1,13 @@
+public class loops {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 5; j++) {
+                if (i == 3) {
+                    break;
+                }
+                System.out.println(i + " " + j);
+            }
+        }
+
+    }
+}
