87d584c7fbb482d396ef2071e8128e20dbb238ea
latha-sree-b <lathasree.balappagari@gmail.com>
2026-09-18 18:58:54 +0530

    MultiplicationTable

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Muliplication.java
@@ -0,0 +1,10 @@
+public class Muliplication {
+    public static void main(String[] args) {
+        int number = 5;
+
+        // Print the first five multiples
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+    }
+}
